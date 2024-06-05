f09018c29fe41573554eb09abe996c2fae29ba22
andy <66240551+flandolf@users.noreply.github.com>
2024-06-05 19:44:23 +1000

    version 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,78 @@
-import { Select, Option, Button, LinearProgress } from '@mui/joy'
-import './App.css'
-import { PitchDetector } from 'pitchy'
-import { useEffect, useState } from 'react'
-import { Note } from 'tonal'
+import "./App.css";
+import { PitchDetector } from "pitchy";
+import { useEffect, useState } from "react";
+import { Note } from "tonal";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { Progress } from "./components/ui/progress";
 const App = () => {
-  const [micDevices, setMicDevices] = useState<string[]>([])
-  const [mic, setMic] = useState<string>('')
-  const [micPerm, setMicPerm] = useState<boolean>(false)
-  const [pitch, setPitch] = useState<string>('')
-  const [closestNote, setClosestNote] = useState<string>('')
-  const [clarity, setClarity] = useState<number>(0)
-  const [centsOff, setCentsOff] = useState<number>(0)
-  const [percentageInTune, setPercentageInTune] = useState<number>(0)
+  const [micDevices, setMicDevices] = useState<string[]>([]);
+  const [mic, setMic] = useState<string>("");
+  const [micPerm, setMicPerm] = useState<boolean>(false);
+  const [pitch, setPitch] = useState<string>("");
+  const [closestNote, setClosestNote] = useState<string>("");
+  const [clarity, setClarity] = useState<number>(0);
+  const [centsOff, setCentsOff] = useState<number>(0);
+  const [percentageInTune, setPercentageInTune] = useState<number>(0);
+  const [running, setRunning] = useState<boolean>(false);
   const permissionName = "microphone" as PermissionName;
   useEffect(() => {
-
     // Check microphone permission status on page load
     navigator.permissions.query({ name: permissionName }).then((result) => {
-      if (result.state === 'granted') {
-        setMicPerm(true)
-      } else if (result.state === 'denied') {
-        setMicPerm(false)
+      if (result.state === "granted") {
+        setMicPerm(true);
+      } else if (result.state === "denied") {
+        setMicPerm(false);
       }
-    })
-    getMicDevices()
-  }, [])
+    });
+    getMicDevices();
+  }, []);
 
   const getMicDevices = () => {
     navigator.mediaDevices
       .enumerateDevices()
       .then((devices) => {
         devices.forEach((device) => {
-          if (device.kind === 'audioinput') {
-            setMicDevices((prev) => [...prev, device.label])
+          if (device.kind === "audioinput") {
+            setMicDevices((prev) => [...prev, device.label]);
             // Remove Duplicates
-            setMicDevices((prev) => prev.filter((v, i, a) => a.indexOf(v) === i))
+            setMicDevices((prev) =>
+              prev.filter((v, i, a) => a.indexOf(v) === i)
+            );
           }
-        })
+        });
       })
       .catch((err) => {
-        console.log(err.name + ': ' + err.message)
-      })
-  }
-
-  const handleChange = (_event: React.SyntheticEvent | null, newValue: string | null) => {
-    setMic(newValue || '')
-  }
+        console.log(err.name + ": " + err.message);
+      });
+  };
 
   const handleMicPerm = () => {
     navigator.permissions.query({ name: permissionName }).then((result) => {
-      if (result.state === 'granted') {
-        setMicPerm(true)
-      } else if (result.state === 'prompt') {
+      if (result.state === "granted") {
+        setMicPerm(true);
+      } else if (result.state === "prompt") {
         navigator.mediaDevices
           .getUserMedia({ audio: true })
           .then(() => {
-            setMicPerm(true)
+            setMicPerm(true);
           })
           .catch((err) => {
-            console.log(err.name + ': ' + err.message)
-          })
-      } else if (result.state === 'denied') {
-        console.log('Permission denied')
+            console.log(err.name + ": " + err.message);
+          });
+      } else if (result.state === "denied") {
+        return;
       }
       result.onchange = () => {
-        console.log(result.state)
-      }
-    })
-  }
+        console.log(result.state);
+      };
+    });
+  };
 
   const updatePitch = (
     analyserNode: AnalyserNode,
@@ -75,98 +80,133 @@ const App = () => {
     input: Float32Array,
     sampleRate: number
   ) => {
-    analyserNode.getFloatTimeDomainData(input)
-    const [pitch, clarity] = detector.findPitch(input, sampleRate)
+    analyserNode.getFloatTimeDomainData(input);
+    const [pitch, clarity] = detector.findPitch(input, sampleRate);
 
     if (clarity >= 0.95) {
-      const frequency = pitch.toFixed(2)
-      const closestNote = Note.fromFreq(parseFloat(frequency))
-      const closestNoteFrequency = Note.freq(closestNote)
+      const frequency = pitch.toFixed(2);
+      const closestNote = Note.fromFreq(parseFloat(frequency));
+      const closestNoteFrequency = Note.freq(closestNote);
 
       // Calculate the difference in frequency in cents
-      const centsOff = 1200 * Math.log2(parseFloat(frequency) / closestNoteFrequency!)
-      const percentageInTune = 1 - Math.abs(centsOff / 50)
-      setPitch(frequency)
-      setClosestNote(closestNote)
-      setClarity(clarity)
-      setCentsOff(centsOff)
-      setPercentageInTune(percentageInTune)
+      const centsOff =
+        1200 * Math.log2(parseFloat(frequency) / closestNoteFrequency!);
+      const percentageInTune = 1 - Math.abs(centsOff / 50);
+      setPitch(frequency);
+      setClosestNote(closestNote);
+      setClarity(clarity);
+      setCentsOff(centsOff);
+      setPercentageInTune(percentageInTune);
     }
 
-    window.setTimeout(() => updatePitch(analyserNode, detector, input, sampleRate), 100)
-  }
+    window.setTimeout(
+      () => updatePitch(analyserNode, detector, input, sampleRate),
+      40
+    );
+  };
 
   const start = () => {
     if (!mic) {
-      console.log('No microphone selected')
-      return
+      return;
     }
 
-    const audioContext = new window.AudioContext()
-    const analyserNode = audioContext.createAnalyser()
+    if (running) {
+      return;
+    }
+
+    setRunning(true);
+
+    const audioContext = new window.AudioContext();
+    const analyserNode = audioContext.createAnalyser();
 
     navigator.mediaDevices
       .getUserMedia({ audio: { deviceId: { exact: mic } } })
       .then((stream) => {
-        audioContext.createMediaStreamSource(stream).connect(analyserNode)
-        const detector = PitchDetector.forFloat32Array(analyserNode.fftSize)
-        const input = new Float32Array(detector.inputLength)
-        updatePitch(analyserNode, detector, input, audioContext.sampleRate)
+        audioContext.createMediaStreamSource(stream).connect(analyserNode);
+        const detector = PitchDetector.forFloat32Array(analyserNode.fftSize);
+        const input = new Float32Array(detector.inputLength);
+        updatePitch(analyserNode, detector, input, audioContext.sampleRate);
       })
       .catch((err) => {
-        console.log(err.name + ': ' + err.message)
-      })
+        console.log(err.name + ": " + err.message);
+      });
 
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-      audioContext.createMediaStreamSource(stream).connect(analyserNode)
-      const detector = PitchDetector.forFloat32Array(analyserNode.fftSize)
-      const input = new Float32Array(detector.inputLength)
-      updatePitch(analyserNode, detector, input, audioContext.sampleRate)
-    })
-  }
+      audioContext.createMediaStreamSource(stream).connect(analyserNode);
+      const detector = PitchDetector.forFloat32Array(analyserNode.fftSize);
+      const input = new Float32Array(detector.inputLength);
+      updatePitch(analyserNode, detector, input, audioContext.sampleRate);
+    });
+  };
 
   return (
-    <>
-      <h1>Pitch Detection</h1>
-      {/* Conditionally render the permission button based on micPerm */}
-      {!micPerm && <Button onClick={handleMicPerm}>Request Microphone Permission</Button>}
-      <Select
-        sx={{
-          margin: '1rem 0 1rem 0'
-        }}
-        onChange={handleChange}
-      >
-        {micDevices.map((device) => (
-          <Option value={device} key={device}>
-            {device}
-          </Option>
-        ))}
-      </Select>
-      <Button onClick={start} disabled={!micPerm}>
-        Start
-      </Button>
-      <h2>Pitch: {pitch} Hz</h2>
-      <h2>
-        Closest Note: {closestNote} - {Note.freq(closestNote)?.toFixed(2)} Hz
-      </h2>
-      <h2>Clarity: {(clarity * 100).toFixed(2)}%</h2>
-      <h2>Cents Off: {centsOff.toFixed(1)}</h2>
-      {centsOff < 0 && <h1>Please Tune Up</h1>}
-      {centsOff > 0 && <h1>Please Tune Down</h1>}
-      <h1>Percentage in Tune:</h1>
-      <div
-        style={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '10px'
-        }}
-      >
-        <LinearProgress determinate variant="plain" value={percentageInTune * 100} />
-        {(percentageInTune * 100).toFixed(2)}%
+    <div className="p-4 text-2xl space-y-2 flex flex-col align-middle justify-center">
+      <div className="flex flex-row">
+        <h1 className="font-semibold text-7xl text-green-500">"Tuner"</h1>
+        <p className="text-2xl">Version 2.0.0</p>
       </div>
-    </>
-  )
-}
+      {!micPerm && (
+        <Button onClick={handleMicPerm}>Request Microphone Permission</Button>
+      )}
+      {micPerm && (
+        <Select
+          onValueChange={(e) => {
+            setMic(e);
+          }}
+        >
+          <SelectTrigger>{mic || "Select Microphone"}</SelectTrigger>
+          <SelectContent>
+            {micDevices.map((device, index) => (
+              <SelectItem key={index} value={device}>
+                {device}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      )}
+      <h2>Microphone Permission: {micPerm ? "Granted" : "Denied"}</h2>
+      <Button onClick={start} disabled={!micPerm || running}>
+        {running ? "Running" : "Start"}
+      </Button>
+      <div className="rounded-lg py-4 w-full">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-8xl font-bold mb-4">
+            <span className="text-green-500">{closestNote}</span>
+          </div>
+          <div className="w-full mb-8">
+            <Progress
+              value={percentageInTune * 100}
+              color={percentageInTune >= 0.5 ? "green" : "red"}
+            />
+          </div>
+          <div className="text-2xl font-medium">
+            <span className="text-green-500">
+              {percentageInTune >= 0.5 ? "In Tune" : "Out of Tune"}
+            </span>{" "}
+            | {centsOff.toFixed(0)} cents
+          </div>
+        </div>
+      </div>
+      <div className="text-sm text-gray-400">
+        <p className="text-base">Raw Data: </p>
+        <p>Pitch: {pitch}</p>
+        <p>Closest Note: {closestNote}</p>
+        <p>Clarity: {clarity}</p>
+        <p>Cents Off: {centsOff}</p>
+        <p>Percentage In Tune: {percentageInTune}</p>
+        <p className="max-w-[40vw]">
+          This app is relatively accurate, but may not be perfect. It works best
+          with a clean tone, however, it can still work with distortion or other
+          effects. It should be used as a tool to help you tune your instrument,
+          but it is not a replacement for a dedicated tuner. The microphone
+          should be line out from your instrument or a microphone close to your
+          instrument. The microphone should not be your computer's built-in
+          microphone.
+        </p>
+        <p className="text-green-500">&copy; 2024 Andy Wang</p>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@fontsource/inter'
+import { ThemeProvider } from './components/theme-provider.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
